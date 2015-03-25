bfbded73668226638fdbaa23b64273f1f4c477e6
Thinh Hung <hungnt.dev@gmail.com>
2015-03-25 16:24:58 +0700

    Add first project

---
+++ tutorial1-firstproject/desktop/src/com/thinhhung/firstproject/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.thinhhung.firstproject.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.thinhhung.firstproject.firstproject;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new firstproject(), config);
+	}
+}
