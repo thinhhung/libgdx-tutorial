0ebc59042b3874b2ab3f293f55256bf5eb42a3ad
Hung Nguyen <hungnt.dev@gmail.com>
2015-05-23 15:15:29 +0700

    Add tutorial2-basicgraphic project

---
+++ tutorial2-basicgraphic/android/src/com/thinhhung/basicgraphic/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.thinhhung.basicgraphic.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.thinhhung.basicgraphic.BasicGraphic;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BasicGraphic(), config);
+	}
+}
