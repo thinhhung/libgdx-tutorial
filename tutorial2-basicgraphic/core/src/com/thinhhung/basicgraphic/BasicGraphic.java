c14f79602f29e02b882975bcc5528f1aadb12e7c
Hung Nguyen <hungnt.dev@gmail.com>
2015-05-26 15:39:30 +0700

    Update basic graphic code

--- tutorial2-basicgraphic/core/src/com/thinhhung/basicgraphic/BasicGraphic.java
+++ tutorial2-basicgraphic/core/src/com/thinhhung/basicgraphic/BasicGraphic.java
@@ -1,27 +1,75 @@
 package com.thinhhung.basicgraphic;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.Timer;
 
-public class BasicGraphic extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+import java.sql.Time;
+
+public class BasicGraphic implements ApplicationListener {
+	private SpriteBatch batch;
+	private TextureAtlas textureAtlas;
+	private Sprite sprite;
+	private int currentFrame = 1;
+	private String currentAtlasKey = new String("0001");
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		textureAtlas = new TextureAtlas((Gdx.files.internal("spritesheet.atlas")));
+		TextureAtlas.AtlasRegion region = textureAtlas.findRegion(currentAtlasKey);
+		sprite = new Sprite(region);
+		sprite.setPosition(120, 100);
+		sprite.scale(2.5f);
+		Timer.schedule(new Timer.Task() {
+			@Override
+			public void run() {
+				currentFrame++;
+				if (currentFrame > 4) {
+					currentFrame = 1;
+				}
+
+				currentAtlasKey = String.format("%04d", currentFrame);
+				sprite.setRegion(textureAtlas.findRegion(currentAtlasKey));
+			}
+		}, 0, 1/5.0f);
 	}
+
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		sprite.draw(batch);
 		batch.end();
+	}
+
+	@Override
+	public void dispose() {
+		batch.dispose();
+		textureAtlas.dispose();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
 	}
 }
